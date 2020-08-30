f01539d1fd78f25a3015cd479509f4cfc195ae8c
Shivam Aggarwal <shivam01101995@gmail.com>
2020-08-30 22:13:48 +0530

    Do dependency injection of CustomerDao in CustomerController to access customer list

--- src/main/java/com/shivam/Controller/CustomerController.java
+++ src/main/java/com/shivam/Controller/CustomerController.java
@@ -1,15 +1,33 @@
 package com.shivam.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.shivam.DAO.CustomerDAO;
+import com.shivam.Entity.Customer;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+	/* This works because in CustomerDaoImpl we gave annotation @Repository, so spring will register it as a bean,
+	 * just like @Controller. @Autowired will do dependency injection of CustomerDaoImpl here.
+	 */
+	@Autowired
+	CustomerDAO customerDao;
+	
 	
 	@RequestMapping("/list")
 	public String listCustomers(Model model) {
+		//Get customer list from Dao
+		List<Customer> customerList = customerDao.getCustomerList();
+		
+		//Add that list to model
+		model.addAttribute("customerList",customerList);
 		
 		return "list-customers"; 
 	}
