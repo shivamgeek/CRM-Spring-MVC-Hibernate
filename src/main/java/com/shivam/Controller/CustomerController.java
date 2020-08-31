baf7c606dc8b5f025217299d4276ecfff0da5dc6
Shivam Aggarwal <shivam01101995@gmail.com>
2020-08-31 15:09:03 +0530

    Added @GetMapping to support HTTP GET method, inplace of @RequestMapping

--- src/main/java/com/shivam/Controller/CustomerController.java
+++ src/main/java/com/shivam/Controller/CustomerController.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.shivam.DAO.CustomerDAO;
 import com.shivam.Entity.Customer;
 
@@ -21,7 +21,8 @@ public class CustomerController {
 	CustomerDAO customerDao;
 	
 	
-	@RequestMapping("/list")
+	//@RequestMapping(path="/list" , method=RequestMethod.GET)
+	@GetMapping("/list")
 	public String listCustomers(Model model) {
 		//Get customer list from Dao
 		List<Customer> customerList = customerDao.getCustomerList();
