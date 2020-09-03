717b771a774de51e74eb04b60e60d08263f50998
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-03 21:06:28 +0530

    Add api mapping for getting single customer

--- src/main/java/com/shivam/rest/CustomerRestController.java
+++ src/main/java/com/shivam/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +18,17 @@ public class CustomerRestController {
 	@Autowired
 	CustomerService custService;
 	
+	//GET list of customers
 	@GetMapping("/customers")
 	public List<Customer> getCustomers(){
 		return custService.getCustomerList();
+	}
+	
+	//GET single customer data using id
+	@GetMapping("/customers/{customerID}")
+	public Customer getCustomer(@PathVariable("customerID") int id) {
+		Customer c = custService.getCustomerUsingID(id);
+		return c;
 	}
 	
 }
