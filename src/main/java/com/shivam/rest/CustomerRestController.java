6718ee6423f6ea79ff7d2db5c1878dbe62c1f059
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-03 22:28:21 +0530

    Added api for POST to add new Customer

--- src/main/java/com/shivam/rest/CustomerRestController.java
+++ src/main/java/com/shivam/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +35,16 @@ public class CustomerRestController {
 		}
 		return c;
 	}
+	
+	//POST to add a new Customer
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer c) {
+		//Set the id explicitly to 0, because in hibernate we're using saveOrUpdate() which checks this ID
+		// if it's empty or 0, then it creates a new customer, else updates existing
+		c.setId(0);
+		custService.saveCustomer(c);
+		return c;
+	}
+	
 	
 }
