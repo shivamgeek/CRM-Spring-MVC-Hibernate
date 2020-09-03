66b442fd55b0cdf6e3feafd5a9b30e8a7f322ae3
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-03 20:52:50 +0530

    Added API endpoint for getting all customers and add jackson to pom.xml

--- src/main/java/com/shivam/rest/CustomerRestController.java
+++ src/main/java/com/shivam/rest/CustomerRestController.java
@@ -1,9 +1,13 @@
 package com.shivam.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shivam.Entity.Customer;
 import com.shivam.Service.CustomerService;
 
 @RestController
@@ -13,7 +17,9 @@ public class CustomerRestController {
 	@Autowired
 	CustomerService custService;
 	
-	
-	
+	@GetMapping("/customers")
+	public List<Customer> getCustomers(){
+		return custService.getCustomerList();
+	}
 	
 }
